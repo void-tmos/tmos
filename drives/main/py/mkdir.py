4e3699bb879039eb79e4e60764a9ae8d3bdaf577
BaguetteYeeter <baguetteyeeter@gmail.com>
2022-09-01 13:03:38 +0100

    Added --parent and -p to mkdir

--- drives/main/py/mkdir.py
+++ drives/main/py/mkdir.py
@@ -4,13 +4,22 @@ except:
     print("mkdir: missing operand")
     raise codeExit("missing operand")
 
+parser = Parser()
+parser.addCmd("-p", "--parent", "parent")
+parser.parse(args)
+args = parser.args
+opts = parser.output
+
 run("getdir -oe '%s' '%s'" % (args[0], getCD()))
 
-try:
-    os.mkdir(file("/".join(directory)))
-except FileExistsError:
-    print("mkdir: cannot create directory '%s': File exists"%args[0])
-except FileNotFoundError:
-    print("mkdir: cannot create directory '%s': No such file or directory"%args[0])
+if opts["parent"]:
+    os.makedirs(file("/".join(directory)), exist_ok=True)
+else:
+    try:
+        os.mkdir(file("/".join(directory)))
+    except FileExistsError:
+        print("mkdir: cannot create directory '%s': File exists"%args[0])
+    except FileNotFoundError:
+        print("mkdir: cannot create directory '%s': No such file or directory"%args[0])
 
 raise codeExit()
